e3246c41d3de611f92fb2b38ed4eeb2ee3903a2c
Itaru7 <itaru.kishikawa@gmail.com>
2022-01-07 22:38:11 +0000

    10.2.1 ユーザーにログインを要求する

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  
   def show
     @user = User.find(params[:id])
   end
@@ -7,7 +9,7 @@ class UsersController < ApplicationController
     @user = User.new
   end
   
- def create
+  def create
     @user = User.new(user_params)
     if @user.save
       log_in @user
@@ -22,7 +24,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   
-   def update
+  def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       # 更新に成功した場合を扱う。
@@ -34,9 +36,18 @@ class UsersController < ApplicationController
   end
 
   private
-
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+    
+    # beforeアクション
+
+    # ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
     end
 end
